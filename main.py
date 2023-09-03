279ecbcd5c56515778be2225d62b20f3c9c3ed15
zabbixserver <videomc1979@gmail.com>
2023-09-03 13:25:14 +0300

    add files

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+
+import requests
+import json
+
+if not os.path.exists('data'):
+    os.mkdir('data')
+
+
+def get_gym():
+    url = 'https://codeforces.com/api/contest.list?gym=true'
+    response = requests.get(url=url).json()
+    with open('data/gym.json', 'w', encoding='utf-8') as file:
+        json.dump(response, file, indent=4, ensure_ascii=False)
+
+
+def get_problems():
+    url = 'https://codeforces.com/api/problemset.problems'
+    response = requests.get(url=url).json()
+    with open('data/problems.json', 'w', encoding='utf-8') as file:
+        json.dump(response, file, indent=4, ensure_ascii=False)
+
+
+def get_tasks():
+    url = 'https://codeforces.com/api/problemset.problems?tags=math'
+    response = requests.get(url=url).json()
+    with open('data/tasks.json', 'w', encoding='utf-8') as file:
+        json.dump(response, file, indent=4, ensure_ascii=False)
+
+
+def main():
+    get_gym()
+    get_problems()
+    get_tasks()
+
+
+if __name__ == '__main__':
+    main()
